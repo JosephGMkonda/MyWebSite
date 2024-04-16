2a652b79e504b85ebd92830b33adb2fbd9181ad3
sangwaniKondowe <josephgmkonda@gmail.com>
2024-04-16 09:57:16 +0400

    first commit

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f)in%hsbcg=ddd&3kc-(60z-(6#36y01yyjosd*qx+i30y-z_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition

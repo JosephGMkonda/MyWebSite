0d568f004ba00b9d1ee18ff2a7aa545b8a7d6d29
sangwaniKondowe <josephgmkonda@gmail.com>
2024-04-27 15:06:25 +0400

    .....

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f)in%hsbcg=ddd&3kc-(60z-(6#36y01yyjosd*qx+i30y-z_i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'MyWeb/static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STORAGES = {
     # ...
     "staticfiles": {
